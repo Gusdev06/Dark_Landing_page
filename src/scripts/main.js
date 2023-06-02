f687b19fb8e8558ba0d6cd5fba6c9eb1d351639e
Gustavo gomes <coven688@exemplo.com>
2023-06-02 10:36:11 -0300

    Criando a sessao de personagens

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,34 +1,64 @@
-
-
 document.addEventListener('DOMContentLoaded', function() {
-    const logosection = document.querySelector('.logo_dark')
+    const buttons = document.querySelectorAll('[data-tab-button]')
+    const text = document.querySelector('.sobre__item__text');
+    
 
     window.addEventListener('load', function() {
-        const logodark = document.querySelector('.logo__dark__item')
+        const logodark = document.querySelector('.logo__dark__item');
         setTimeout(function () {
-            logodark.style.opacity = 1.7
-            logodark.style.transform = 'scale(1.05)'
-        },500)
-    })
+            logodark.style.opacity = 1.7;
+            logodark.style.transform = 'scale(1.05)';
+        }, 500);
+    });
 
-    function reveal() {
-        var reveals = document.querySelectorAll('.sobre__item')
 
-        reveals.forEach((reveal) => {
-            let windowHeight = window.innerHeight
-            let elementTop = reveal.getBoundingClientRect().top
-            let elementVisible = 100;
+        for (let i = 0; i < buttons.length; i++) {
+            buttons[i].addEventListener('click', function (botao) {
+                const abaAlvo  = botao.target.dataset.tabButton;
+                const aba = document.querySelector(`[data-tab-id="${abaAlvo}"]`)
+                escondeAbas()
+                aba.classList.add('personagens__list--is-active')
+            })
+        }
 
-            if(elementTop < windowHeight - elementVisible) {
-                reveal.classList.add('animar')
-            } else {
-                reveal.classList.remove('animar')
+        function escondeAbas() {
+            const tabsContainer = document.querySelectorAll('[data-tab-id]')
+
+            for(let i = 0; i < tabsContainer.length; i++) {
+                tabsContainer[i].classList.remove('personagens__list--is-active')
             }
-            
-        })
-        window.addEventListener('scroll', reveal)
-    }
+        }
 
-    reveal();
-})
-
+    
+        function reveal() {
+        const reveals = document.querySelectorAll('.sobre__item');
+        const windowHeight = window.innerHeight;
+        const elementVisible = 100;
+    
+        reveals.forEach((reveal) => {
+            const elementTop = reveal.getBoundingClientRect().top;
+    
+            if (elementTop < windowHeight - elementVisible) {
+            reveal.classList.add('animar');
+            } else {
+            reveal.classList.remove('animar');
+            }
+        });
+        }
+    
+        function maquinaDeEscrever(elemento) {
+        const textArray = elemento.innerHTML.split('');
+        elemento.innerHTML = '';
+        textArray.forEach((letra, i) => {
+            setTimeout(function() {
+            elemento.innerHTML += letra;
+            }, 25 * i);
+        });
+        }
+    
+        maquinaDeEscrever(text);
+        reveal();
+    
+        // Chama a função reveal() quando a página é rolada
+        window.addEventListener('scroll', reveal);
+    });
